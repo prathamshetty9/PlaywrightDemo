774e0d29c33474df7364c54884286308084232b2
prathamshetty9 <prathamshetty6699@gmail.com>
2024-12-24 14:09:52 +0530

    Change the Browser Context Use Case

--- tests/lamda-test/browser-context/browser-context-flow-test.spec.ts
+++ tests/lamda-test/browser-context/browser-context-flow-test.spec.ts
@@ -23,10 +23,15 @@ test("Browser Context Application. @LAMDA", async () => {
      const emailId1:Locator = page1.locator('#input-email');
      const password1:Locator = page1.locator('#input-password');
      const loginBtn1:Locator = page1.locator("[value='Login']");
+     const emailInputField:Locator = page1.getByRole('textbox', { name: 'E-Mail' });
+     const editAccountInfo:Locator = page1.locator("//div[contains(@class,'row')]//a[contains(text(),'account information')]");
+
     // Admin Credentials 
-     await emailId1.fill("pwtest@opencart.com");
-     await password1.fill("playwright@123");
+     await emailId1.fill("adminuser123@abc.com");
+     await password1.fill("Admin@1234");
      await loginBtn1.click();
+     await editAccountInfo.click();
+     expect(await emailInputField.inputValue()).toBe("adminuser123@abc.com");
 
     //browser 2
     await page2.goto(`https://naveenautomationlabs.com/opencart/index.php?route=account/login`, {
@@ -36,11 +41,15 @@ test("Browser Context Application. @LAMDA", async () => {
     const emailId2:Locator = page2.locator('#input-email');
     const password2:Locator = page2.locator('#input-password');
     const loginBtn2:Locator = page2.locator("[value='Login']");
+    const emailInputField2:Locator = page2.getByRole('textbox', { name: 'E-Mail' });
+    const editAccountInfo2:Locator = page2.locator("//div[contains(@class,'row')]//a[contains(text(),'account information')]");
+
     // User Credentials 
     await emailId2.fill("userpw@pw.com");
     await password2.fill("Test@123");
     await loginBtn2.click();
-    
+    await editAccountInfo2.click();
+    expect(await emailInputField2.inputValue()).toBe("userpw@pw.com");
     
 
     //  await new Promise(()=>{});
